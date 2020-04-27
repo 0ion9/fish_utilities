cb935b0f444c4172c7b64fc8fd8c2e03867f4f53
David Gowers <finticemo@gmail.com>
2020-04-28 09:27:08 +0930

    reloadfunc: cope with the possibility that a function was defined in a file but that file has since been removed.

--- functions/reloadfunc.fish
+++ functions/reloadfunc.fish
@@ -10,7 +10,14 @@ function reloadfunc -w functions -d "Reload specified functions, if they were de
     end
     set -l where (functions -D $funcname)
     if not contains $where n/a stdin
-      source $where
+      if not test -e $where
+        # this can occur when defining functions by editing then sourcing files in /tmp/
+        # (similar to what `funced` does)
+        echo "'$funcname' was defined in '$where', but this file was not found on disk." 1>&2
+        set retval 1
+      else
+        source $where
+      end
     else
       set retval 1
       echo "'$funcname' can't be reloaded as it was defined in ($where), not a file." 1>&2
