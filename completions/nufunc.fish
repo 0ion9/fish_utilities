f469158f411c360842c8549754f5748fd34cd824
David Gowers <finticemo@gmail.com>
2020-04-28 11:08:14 +0930

    completions/nufunc : don't use sed

--- completions/nufunc.fish
+++ completions/nufunc.fish
@@ -1,7 +1,7 @@
 function __fish_print_user_functions
-  find ~/.config/fish/functions -printf '%P\n'| sed 's/[.]fish//'
+  find ~/.config/fish/functions -printf '%P\n'| string replace -r '.fish$' ''
 end
 
-complete -c nufunc -f -a '(__fish_print_user_functions)'
+complete -c nufunc -x -a '(__fish_print_user_functions)'
 complete -c funcname -w nufunc
 complete -c reloadfunc -w nufunc
