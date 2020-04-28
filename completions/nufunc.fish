19134733951761f9effa794b3661706bbfae1af1
David Gowers <finticemo@gmail.com>
2020-04-28 11:15:25 +0930

    completions (funcfile, reloadfunc) : complete ALL functions, not only user functions, unlike nufunc.

--- completions/nufunc.fish
+++ completions/nufunc.fish
@@ -2,6 +2,18 @@ function __fish_print_user_functions
   find ~/.config/fish/functions -printf '%P\n'| string replace -r '.fish$' ''
 end
 
+# from 'functions' completion; unfortunately we can't just call the one it defines.
+
+function __rf_fish_maybe_list_all_functions
+    # if the current commandline token starts with an _, list all functions
+    if string match -qr -- '^_' (commandline -ct)
+        functions -an
+    else
+        functions -n
+    end
+end
+
+
 complete -c nufunc -x -a '(__fish_print_user_functions)'
-complete -c funcname -w nufunc
-complete -c reloadfunc -w nufunc
+complete -c reloadfunc -x -a '(__rf_fish_maybe_list_all_functions)'
+complete -c funcname -w reloadfunc
